2d3032d8dedfdfd310938f325c0aa22a919eb93a
example <email@example.com>
2024-08-22 18:01:47 +0300

    push

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -4,50 +4,40 @@ import Footer from "@/Components/Footer";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { FaInstagram, FaLinkedinIn, FaGithub } from "react-icons/fa";
-
 const Page = ({ params }) => {
   const [data, setData] = useState(null);
 
-  const fetchBlogData = useCallback(async () => {
-    try {
-      const response = await axios.get("/api/blog", {
-        params: {
-          id: params.id,
-        },
-      });
-      setData(response.data);
-    } catch (error) {
-      console.error("Error fetching blog data:", error);
-    }
-  }, [params.id]); 
+  const fetchBlogData = async () => {
+    const response = await axios.get("/api/blog", {
+      params: {
+        id: params.id,
+      },
+    });
+    setData(response.data);
+  };
 
-  useEffect(() => {
-    fetchBlogData();
-  }, [fetchBlogData]);
 
-  if (!data) {
-    return <p>Loading...</p>; 
-  }
-
-  return (
+  return data ? (
     <>
       <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28">
-        <div className="flex items-center">
+        <div className="flex  items-center">
           <Link href="/">
             <button className="relative inline-flex items-center justify-center p-4 rounded-lg overflow-hidden bg-black text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2">
               <span className="absolute inset-0 bg-gray-800 opacity-50 group-hover:opacity-75 transition-opacity duration-300"></span>
               Makro..
             </button>
           </Link>
+         
         </div>
         <div className="text-center my-24">
           <h1 className="text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto">
             {data.title}
           </h1>
+          
           <Image
-            className="mx-auto mt-6 border border-white rounded-full"
+            className="mx-auto mt-6 border border-white rounded-full "
             src={assets.profile_icon}
             alt=""
             width={60}
@@ -66,10 +56,12 @@ const Page = ({ params }) => {
           width={1280}
           height={720}
         />
+
         <div
           className="blog-content"
           dangerouslySetInnerHTML={{ __html: data.description }}
         ></div>
+
         <div className="my-24">
           <p className="black font-semibold my-4">Sosyal medya hesaplarım</p>
           <div className="flex gap-4">
@@ -102,6 +94,8 @@ const Page = ({ params }) => {
       </div>
       <Footer />
     </>
+  ) : (
+    <></>
   );
 };
 
